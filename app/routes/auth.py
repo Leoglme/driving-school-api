87fdc9cbf1691b45d0f8eb114e55c1c9c06cc23d
anthony <anthony.cordon@epitech.eu>
2022-06-30 14:54:38 +0200

    lougout WIP

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 import jwt
 import redis
-from flask import request, redirect, request, render_template, make_response
+from flask import request, redirect, request, render_template, make_response, current_app
 from werkzeug.security import check_password_hash
 from flask import jsonify
 
@@ -12,7 +12,7 @@ from ..models.Token import Token
 from ..models.User import User
 from .index import router
 from flask import Flask, request, render_template
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 
 
@@ -38,22 +38,32 @@ def login():
     return make_response('Could not verify password!', 403, {'WWW-Authenticate': 'Basic-realm= "Wrong Password!"'})
 
 
+# @router.route("/logout/<int:user_id>", methods=["DELETE"])
+# @jwt_required()
+# def logout(user_id):
+#     user = request.get_json()
+#     token = request.get_json()
+#     if token.get('token') and user.get('user_id'):
+#         db.session.delete(token)
+#     return make_response(user_id, 204)
+
+ACCESS_EXPIRES = timedelta(hours=1)
+
 jwt_redis_blocklist = redis.StrictRedis(
     host="localhost", port=6000, db=0, decode_responses=True
 )
 
-ACCESS_EXPIRES = timedelta(hours=1)
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
 
 
 @router.route("/logout", methods=["DELETE"])
 @jwt_required()
 def logout():
-    token = get_jwt()
-    jti = token["jti"]
-    ttype = token["type"]
+    jti = get_jwt()["jti"]
     jwt_redis_blocklist.set(jti, "", ex=ACCESS_EXPIRES)
-
-    # Returns "Access token revoked" or "Refresh token revoked"
-    return jsonify(msg=f"{ttype.capitalize()} token successfully revoked")
-
-
+    return jsonify(msg="Access token revoked")
